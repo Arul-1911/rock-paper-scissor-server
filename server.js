c983c7d4053981d6d48dda9e6eaa91b3a35b18fb
Arul <arulpraveen19112002@gmail.com>
2024-10-04 21:03:43 +0530

    port added

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use("/api", gameRoutes);
 
 //start the server
-const PORT = process.env.PORT || 5000;
+const PORT = 5050;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
